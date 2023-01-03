73928d459af2502394b6b56b62376c120cd1ece2
eric-lab-star <cyon256@icloud.com>
2023-01-03 15:23:21 +0900

    mini quiz game

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Eric-lab-star/quiz-game
+
+go 1.19
--- main.go
+++ main.go
@@ -1,7 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("quiz game")
+	csvFilename := flag.String("csv", "problems.csv", "a csv file name in the format of 'questions,answer'")
+	flag.Parse()
+	file, err := os.Open(*csvFilename)
+	if err != nil {
+		exit(fmt.Sprintf("failed to open CSV file: %s\n", *csvFilename))
+	}
+	r := csv.NewReader(file)
+	lines, err := r.ReadAll()
+	if err != nil {
+		exit("failed to parse provided file")
+	}
+	parsedLines := ParseLines(lines)
+	correct := 0
+	for i, p := range parsedLines {
+		fmt.Printf("problem #%d: %s=\n", i+1, p.q)
+		var answer string
+		fmt.Scanf("%s\n", &answer)
+		if answer == p.a {
+			correct++
+			fmt.Println("correct")
+		} else {
+			fmt.Println("incorrect")
+		}
+	}
+	fmt.Printf("You scored %d.\n", correct)
+
+}
+
+func ParseLines(lines [][]string) []Problems {
+	ret := make([]Problems, len(lines))
+	for index, values := range lines {
+		ret[index] = Problems{
+			q: values[0],
+			a: values[1],
+		}
+	}
+	return ret
+}
+
+type Problems struct {
+	q string
+	a string
+}
+
+func exit(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
 }
